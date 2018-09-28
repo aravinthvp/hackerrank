c7a397b6ff3143599de6a9d2b00d691df8bb6f18
aravinthvp <34703046+aravinthvp@users.noreply.github.com>
2018-09-28 07:37:14 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,9 @@
+Read an integer N . For all non-negative integers i<N , print i***2. See the sample for details. 
+
+Solution:
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
