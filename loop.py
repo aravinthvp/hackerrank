a3479a94c406336d78931a3c4c79bd0dea04ac54
aravinthvp <34703046+aravinthvp@users.noreply.github.com>
2018-09-28 14:53:49 +0530

    Add files via upload

--- loop.py
+++ loop.py
@@ -1,9 +1,22 @@
-Read an integer N . For all non-negative integers i<N , print i***2. See the sample for details. 
+Task
+You are given the year, and you have to write a function to check if the year is leap or not.
 
-Solution:
+Note that you have to complete the function and remaining code is given as template.
 
-if __name__ == '__main__':
-    a = int(input())
-    b = int(input())
-    print(a//b)
-    print(a/b)
+Solution
+
+
+def is_leap(year):
+    leap = False
+    if year%400==0:
+        return True
+    elif year%100==0:
+        return False
+    elif year%4==0:
+        return True
+    
+    return leap
+
+
+year = int(raw_input())
+print is_leap(year)
